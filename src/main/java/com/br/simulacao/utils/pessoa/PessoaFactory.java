780f94ee397bdb81cc021a77d83e3f58f66dc272
Fabiana Alencar <fabianabyte@gmail.com>
2023-02-07 20:56:31 -0300

    Implementação de fábrica de pessoa

--- src/main/java/com/br/simulacao/utils/pessoa/PessoaFactory.java
+++ src/main/java/com/br/simulacao/utils/pessoa/PessoaFactory.java
@@ -1,7 +1,9 @@
 package com.br.simulacao.utils.pessoa;
 
 import com.br.simulacao.domain.entity.PessoaEntity;
+import com.br.simulacao.domain.model.api.pessoa.TipoPessoa;
 import com.br.simulacao.utils.identificacao.IdentificacaoFactory;
+import com.br.simulacao.utils.identificacao.domain.model.TipoIdentificacao;
 
 public class PessoaFactory {
     private PessoaFactory(){}
@@ -9,6 +11,24 @@ public class PessoaFactory {
     public static PessoaEntity gerarPessoaEntity(String identificacao){
         return PessoaEntity.builder()
                 .tipoPessoa(IdentificacaoFactory.getIdentificacao(identificacao).getTipoIdentificacao().getDescricao())
+                .identificacao(identificacao)
+                .build();
+    }
+    public static PessoaEntity gerarPessoaFisicaEntity(String identificacao){
+        return PessoaEntity.builder()
+                .tipoPessoa(TipoPessoa.CPF.getDescricao())
+                .identificacao(identificacao)
+                .build();
+    }
+    public static PessoaEntity gerarPessoaJuridicaEntity(String identificacao){
+        return PessoaEntity.builder()
+                .tipoPessoa(TipoPessoa.CNPJ.getDescricao())
+                .identificacao(identificacao)
+                .build();
+    }
+    public static PessoaEntity gerarPessoaEntity(String identificacao, TipoIdentificacao tipoIdentificacao){
+        return PessoaEntity.builder()
+                .tipoPessoa(tipoIdentificacao.getDescricao())
                 .identificacao(identificacao)
                 .build();
     }
