0ab469d53ff43ef44de6dde039e7ba914abb5d65
Fabiana Alencar <fabianabyte@gmail.com>
2023-02-11 09:47:32 -0300

    Implementação de testes unitários.

--- src/test/java/com/br/simulacao/utils/cliente/PessoaFactoryTest.java
+++ src/test/java/com/br/simulacao/utils/cliente/PessoaFactoryTest.java
@@ -1,22 +1,47 @@
 package com.br.simulacao.utils.cliente;
 
+import com.br.simulacao.domain.entidade.pessoa.PessoaEntidade;
+import com.br.simulacao.domain.model.identificacao.TipoIdentificacao;
+import com.br.simulacao.utils.pessoa.PessoaFactory;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PessoaFactoryTest {
 
+    private String identificacao = "12345678901234";
+
     @Test
     void gerarPessoaEntity() {
+        PessoaEntidade pessoaGerada = PessoaFactory.gerarPessoaPorIdentificacao(identificacao);
+
+        assertEquals(identificacao, pessoaGerada.getIdentificacao());
+        assertNotNull(pessoaGerada.getIdentificacao());
     }
 
     @Test
     void gerarPessoaFisicaEntity() {
+        PessoaEntidade pessoaGerada = PessoaFactory.gerarPessoaFisica(identificacao);
+
+        assertEquals(identificacao, pessoaGerada.getIdentificacao());
+        assertNotNull(pessoaGerada.getIdentificacao());
     }
 
     @Test
     void gerarPessoaJuridicaEntity() {
+        PessoaEntidade pessoaGerada = PessoaFactory.gerarPessoaJuridica(identificacao);
+
+        assertEquals(identificacao, pessoaGerada.getIdentificacao());
+        assertNotNull(pessoaGerada.getIdentificacao());
     }
 
     @Test
     void testGerarPessoaEntity() {
+        PessoaEntidade pessoaGerada = PessoaFactory.gerarPessoa(identificacao, TipoIdentificacao.CNPJ);
+
+        assertEquals(identificacao, pessoaGerada.getIdentificacao());
+        assertNotNull(pessoaGerada.getIdentificacao());
     }
+
 }
